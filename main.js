dd819f2061a7277fd64eaabd7c94b9dcd7457777
= <shivampathak19972@gmail.com>
2026-01-20 22:12:12 +0530

    Test

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  // TODO: Implement this function
+  let body = '';
+
+  req.on('data', chunk => {
+    body += chunk.toString();
+  });
+
+  req.on('end', () => {
+    const responseData = {
+      method: req.method,
+      url: req.url,
+      headers: req.headers,
+      body: body || null
+    };
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(responseData, null, 2));
+  });
 });
 
 server.listen(3000, () => {
